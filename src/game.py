07cf52995647bac7dfa11d162b663ad2e82d2008
asrouji <asrouji@lion.lmu.edu>
2021-12-07 18:34:14 -0800

    game works

--- src/game.py
+++ src/game.py
@@ -11,6 +11,7 @@ class Game():
     LOCATION_LIST = ["Market", "Gym", "Beach", "Coffee Shop", "Park", "Office"]
 
     def __init__(self):
+        self.round = 0
         self.players = self.getPlayers()
         self.impostor = random.choice(self.NPC_LIST)
         self.interactions = self.generateInteractions()
@@ -30,7 +31,7 @@ class Game():
         for player in self.NPC_LIST:
             temp = self.NPC_LIST.copy()
             temp.remove(player)
-            for i in range(random.randint(1, 3)):
+            for i in range(random.randint(2, 3)):
                 randomPlayer = random.choice(temp)
                 randomLocation = random.randint(0, len(self.LOCATION_LIST) - 1)
                 interactions[player][randomPlayer] = [
@@ -57,7 +58,7 @@ class Game():
                                                   self.LOCATION_LIST[location])
                     interactions[key][npc][1] += (" at the " +
                                                   self.LOCATION_LIST[location])
-                    if not break_at_end and random.randint(0, 3) == 1:
+                    if not break_at_end and random.randint(0, 3) == 1 and interactions[npc][key][2] == 'No Intel':
                         interactions[npc][key][2] = current + \
                             " is not the impostor"
                         interactions[key][npc][2] = current + \
@@ -74,30 +75,45 @@ class Game():
     def play(self):
         clear()
         while True:
+            self.round += 1
             for player in self.players:
                 try:
-                    print(game.interactions)
+                    # print(game.interactions)
+                    print("NPCs: ", end="")
+                    for i in range(len(self.NPC_LIST)):
+                        npc = self.NPC_LIST[i]
+                        print(npc, end=" ")
+                    print("")
                     print("Locations: ", end="")
                     for i in range(len(self.LOCATION_LIST)):
                         location = self.LOCATION_LIST[i]
                         print("[" + str(i+1) + "] " + location, end="  ")
                     print("\n")
-                    npcs = input(player + ", NPCs to listen to: ").upper()
+                    npcs = input(player + ", NPCs to listen to (separated by space): ").upper()
                     npcs = npcs.split()
-                    location = int(input(player + ", Location of NPCs: "))
+                    if len(npcs) > 2:
+                        location = npcs[2]
+                    else: 
+                        location = int(input(player + ", Location of NPCs (use # above): "))
                     print("")
                     if npcs[0][0] in self.interactions[npcs[1][0]] and self.interactions[npcs[1][0]][npcs[0][0]][0] + 1 == location:
                         print(self.interactions[npcs[1][0]][npcs[0][0]][1])
                         print(self.interactions[npcs[1][0]][npcs[0][0]][2])
                     else:
-                        print('Conversation is Private\n')
-                    input('\nPress Enter to continue...')
+                        print('Conversation is Private')
+                    cont = input('\nPress Enter to continue, or G to guess the impostor: ')
                     clear()
+                    if (cont.upper() == 'G'):
+                        imp = input('Who is the impostor? ')
+                        if imp.upper()[0] == self.impostor:
+                            print('The winner is ' + player + '!')
+                            return
+                        else:
+                            print('Incorrect')
                 except:
                     input('Error occured. Press Enter to continue...')
                     clear()
                     continue
-
 
 clear()
 game = Game()
